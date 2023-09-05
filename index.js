ce0f3ab33333cc1438ecd0e114de016d83547359
doyekim <dovey_0_@naver.com>
2023-09-05 12:50:20 +0900

    dada

--- index.js
+++ index.js
@@ -8,26 +8,49 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const data = {
+  select : function () {
+    return JSON.parse(fs.readFileSync('./test.json'))
+  },
+  insert: function (newObj) {
+    const jsonData = data.select();
+    let newData = [...jsonData, { id: jsonData.length + 1, ...newObj }]
+    // ... => 한꺼풀 벗긴다는 의미로 생각하면 편할듯
+    fs.writeFileSync('./test.json', JSON.stringify(newData));
+    return newData;
+  },
+  update: function () { },
+  delete: function () { }
+
+}
+
+
+
+
 app.get('/abc', function (req, res) {
-  const jsonData = fs.readFileSync('./test.json');
-  res.send( JSON.parse(jsonData) )
+/*   const jsonData = fs.readFileSync('./test.json');
+  res.send( JSON.parse(jsonData) ) */
+  res.send(data.select());
 })
 
-
-app.get('/abc/:id', function (req, res) {
-  const jsonData = fs.readFileSync('./test.json');
-  const data = JSON.parse(jsonData);
-
+app.delete('/abc/:id', function (req, res) {
+  const jsonData = data.select();
+  // const data = JSON.parse(jsonData);
   const {id} = req.params;
-  const aaa = data.filter( n => n.id == id)
-  res.send(aaa)
+  const delData = jsonData.filter( n => n.id != id)
+
+  fs.writeFileSync('./test.json', JSON.stringify(delData));
+
+  res.send(delData)
 })
-
-
 
 app.post('/insert', function (req, res) {
-  console.log(req.body)
-  const jsonData = fs.writeFileSync('./test.json', JSON.stringify(req));
-  res.send('성공');
+
+/*   console.log(req.body)
+  const jsonData = JSON.parse(fs.readFileSync('./test.json'));
+  let data = [...jsonData, { id: jsonData.length+1 , ...req.body }]
+  // ... => 한꺼풀 벗긴다는 의미로 생각하면 편할듯
+  fs.writeFileSync('./test.json', JSON.stringify(data)); */
+  res.send(data.insert(req.body));
 })
 app.listen(3000)
